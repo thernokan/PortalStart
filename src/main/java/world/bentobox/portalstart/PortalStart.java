7a419a054c5a9861e55259f0f982020aa7634887
tastybento <tastybento@users.noreply.github.com>
2024-08-18 17:19:35 -0700

    Initial idea

--- src/main/java/world/bentobox/portalstart/PortalStart.java
+++ src/main/java/world/bentobox/portalstart/PortalStart.java
@@ -1,9 +1,18 @@
 package world.bentobox.portalstart;
 
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPortalEvent;
 
 import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.user.User;
 
 
 /**
@@ -12,6 +21,8 @@ import world.bentobox.bentobox.api.addons.Addon;
  *
  */
 public class PortalStart extends Addon implements Listener {
+
+    private final Set<UUID> inTeleport = new HashSet<>();
 
     @Override
     public void onEnable() {
@@ -24,6 +35,34 @@ public class PortalStart extends Addon implements Listener {
         // Do nothing
     }
 
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPortal(PlayerPortalEvent e) {
+        // Check if the destination is a game world and the source is out of the world
+        if (e.getTo() != null) {
+            if (getPlugin().getIWM().inWorld(e.getTo()) && !getPlugin().getIWM().inWorld(e.getFrom())) {
+                String perm = getPlugin().getIWM().getPermissionPrefix(e.getTo().getWorld()) + "island.portalstart";
+                User user = User.getInstance(e.getPlayer());
+                if (!user.hasPermission(perm)) {
+                    return;
+                }
+                // Check if the player has an island in this world
+                if (getIslands().hasIsland(e.getTo().getWorld(), e.getPlayer().getUniqueId())) {
+                    // Teleport to their island
+                    e.setTo(getIslands().getHomeLocation(e.getTo().getWorld(), user));
+                    return;
+                }
 
+                // Get the Game Mode
+                getPlugin().getIWM().getAddon(e.getTo().getWorld()).ifPresent(gm -> {
+                    e.setCancelled(true);
+                    inTeleport.add(e.getPlayer().getUniqueId());
+                    gm.getPlayerCommand().ifPresent(plCmd -> {
+                        plCmd.call(user, "", List.of());
+                    });
+                });
+
+            }
+        }
+    }
 
 }
